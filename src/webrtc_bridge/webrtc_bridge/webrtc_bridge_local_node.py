e8d4c681bd00984f615248b9f0369e377719c58c
Andrey Tatarinov <a@tatarinov.co>
2025-06-22 21:11:20 +0000

    add debug launch file and stub node

--- src/webrtc_bridge/webrtc_bridge/webrtc_bridge_local_node.py
+++ src/webrtc_bridge/webrtc_bridge/webrtc_bridge_local_node.py
@@ -1,6 +1,22 @@
+import rclpy
+from rclpy.node import Node
+
+
+class WebRTCBridgeLocalNode(Node):
+    def __init__(self):
+        super().__init__("webrtc_bridge_local_node")
+        self.get_logger().info("Hello World")
+
+
 def main():
-    print('Hi from webrtc_bridge.')
+    rclpy.init()
+    node = WebRTCBridgeLocalNode()
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
